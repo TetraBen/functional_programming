3dde026e1e8cc6bc743608a0d293b43e08ec9b64
TetraBen <ben_azouelos@hotmail.com>
2017-10-20 22:08:12 +0100

    Exercise 3.10-12: foldLeft (tail recursive) and reverse

--- src/dataStructures/List.scala
+++ src/dataStructures/List.scala
@@ -1,6 +1,8 @@
 package dataStructures
 
-sealed trait List[+A]
+sealed trait List[+A] {
+  def length : Int = List.foldLeft(this, 0)((acc, _) => 1 + acc)
+}
 case object Nil extends List[Nothing]
 case class Cons[A](head:A, tail:List[A]) extends List[A]
 
@@ -11,16 +13,35 @@ object List {
     case Cons(x, xy) => f(x, foldRight(xy,z)(f))
   }
 
-  def sum(xs:List[Int]) : Int = foldRight(xs, 0)(_ + _)
-  def product(xs:List[Double]) : Double = foldRight(xs, 0.0)(_*_)
-  def length[A](xs:List[A]) : Int = foldRight(xs, 0)((_, acc) => 1 + acc)
+  def foldLeft[A,B](xs:List[A],z:B)(f: (B,A) => B) : B = {
 
+    @annotation.tailrec
+    def go(xs:List[A],acc:B)(f: (B,A) => B) : B = xs match {
+      case Cons(x,xy) => go(xy,f(acc,x))(f)
+      case Nil => acc
+    }
+    go(xs,z)(f)
+  }
+
+
+  // def sum(xs:List[Int]) : Int = foldRight(xs,0)(_ + _)
+  def sum(xs:List[Int]) : Int = foldLeft(xs,0)(_ + _)
+
+  // def product(xs:List[Double]) : Double = foldRight(xs, 1.0)(_*_)
+  def product(xs:List[Double]) : Double = foldLeft(xs, 1.0)(_*_)
+
+  //def length[A](xs:List[A]) : Int = foldRight(xs, 0)((_, acc) => 1 + acc)
+  def length[A](xs: List[A]) : Int = foldLeft(xs, 0)((acc, _) => 1 + acc)
   // Tail of Nil being Nil is probably wrong. Could return an option here,
   // but for now just return an empty list
   def tail[A](xs:List[A]) : List[A] = xs match {
     case Cons(_, xy) => xy
     case Nil => Nil
   }
+
+  def reverse[A](xs:List[A]) : List[A] = foldLeft(xs, Nil:List[A])(
+    (acc, x) => Cons(x, acc)
+  )
 
   def setHead[A](x:A, xs:List[A]) : List[A] = xs match {
     case Cons(_, xy) => Cons(x, xy)
